4a30a3fc78e0fb73bfe19782f621921f0ba9480f
squareroot38 <Bourabh@Sourabhs-MacBook-Air.local>
2024-04-07 23:47:30 +0530

    added helloworld

---
+++ helloworld/README.md
@@ -0,0 +1,18 @@
+# helloGO
+
+hello world with GoLang.
+
+prerequisites 
+1. Go installed on ur PC.
+2. IDE installed , preferably VS code.
+
+
+visit link to download and install GO on your machine
+https://go.dev/dl/ 
+
+visit link to download VS code 
+https://code.visualstudio.com/download
+
+
+
+
---
+++ helloworld/helloworld.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+// 'import' is a keyword in Go 
+// import in Go  == #include in C
+// gives [package main] access to the functions from "fmt"
+// import links 'fmt' package with package main
+// 'fmt' == <stdio.h>
+// fmt package contains functions for printing formatted output and scanning input
+// fmt default Go standard library 
+// fmt short for format
+
+func main() {
+
+// execution of program begins in func main() 
+// between flower brackets is the body of program
+// { body of program }
+
+fmt.Println("Hello World")
+
+}
